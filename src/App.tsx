d1d66f61555e9c1877d2d1fa79c9566591178650
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-03-13 13:25:03 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [code, setCode] = useState("");
+  
+  function changeCode(ev: React.ChangeEvent<HTMLTextAreaElement>){
+    setCode(ev.target.value);
+  }
+
+  function run(){
+    eval(code);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea value={code} onChange={(ev) => changeCode(ev)}></textarea>
+      <button onClick={run}>Run Code</button>
+      <div className='output-code'>
+
+      </div>
     </div>
   );
 }
